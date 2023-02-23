84fe237191a5e0759bbc9fd649ef4c4c04e34cf3
alex308248 <alex308248@gmail.com>
2023-02-22 23:54:58 -0500

    GymBro

---
+++ GymBro/GymBroApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymBroApp.swift
+//  GymBro
+//
+//  Created by user236772 on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct GymBroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
