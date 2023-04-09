cd5ebcdd3f0b3630472b0e2f97f7d084192b6cbf
Alex Chao <alex308248@gmail.com>
2023-04-09 17:41:33 -0400

    documentation on MainView

--- GymBro/Views/MainView.swift
+++ GymBro/Views/MainView.swift
@@ -10,17 +10,18 @@
 
 import SwiftUI
 
-enum Tab {
-    case Chart
-    case History
-    case Routine
-    case Info
-    case Setting
-}
 
+
+/// The very first view after starting the APP
 struct MainView: View {
     
+    /// The control variable for the FiveTabView
+    ///
+    /// It will change the defualt view of the tab view inside the FiveTabView
     @State private var selectedTab: Tab = .Routine
+    
+    
+    // MARK: BODY
     
     var body: some View {
         NavigationView {
@@ -29,42 +30,59 @@ struct MainView: View {
     }
 }
 
+
+// MARK: COMPONENT
+
+/// the enum type for the Tab of tab bar
+///
+/// - Warning: I will keep this enum Tab in MainView for a while for researching a better arrangement
+enum Tab {
+    case Chart
+    case History
+    case Routine
+    case Info
+    case Setting
+}
+
+/// The tab view for five modules
+///
+/// Thing to do:
+/// 1. How to extend the showing View from only one View to multiple view in one tab
+/// - Warning: I will keep the view in the MainView for a while for a better arrangement!
 struct FiveTabView: View {
     
+    /// Tab controler
+    ///
+    /// A required parameter when init FiveTabView
     @State var selectedTab: Tab
     
     var body: some View {
         TabView(selection: $selectedTab) {
-            // ChartViews module
+            
             MainChartView()
                 .tabItem {
                     Label("Chart", systemImage: "chart.xyaxis.line")
                 }
                 .tag(Tab.Chart)
             
-            // HistoryView module
             IntroHistoryView()
                 .tabItem {
                     Label("History", systemImage: "book")
                 }
                 .tag(Tab.History)
             
-            
-            // RoutineViews module
             NewRoutineView()
                 .tabItem {
                     Label("Routine", systemImage: "dumbbell")
                 }
                 .tag(Tab.Routine)
             
-            // InfoViews module
             MainInfoView()
                 .tabItem {
                     Label("Info", systemImage: "person.fill")
                 }
                 .tag(Tab.Info)
             
-            // SettingViews module
             MainSettingView()
                 .tabItem {
                     Label("Setting", systemImage: "gear")
@@ -73,6 +91,9 @@ struct FiveTabView: View {
         }
     }
 }
+
+
+// MARK: PREVIEW
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
