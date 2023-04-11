83be58d2f69eab86caa2e9bff87bce66a756742c
Alex Chao <alex308248@gmail.com>
2023-04-10 21:52:23 -0400

    Add a new screen in RSelectionView for adding new routine

--- GymBro/Views/RoutineViews/RoutineSelectionVIew.swift
+++ GymBro/Views/RoutineViews/RoutineSelectionVIew.swift
@@ -16,34 +16,76 @@ struct RoutineSelectionVIew: View {
         "Back Day",
     ]
     
+    @State var addRoutine: Bool = false
+    
     var body: some View {
-        ScrollView(.vertical, showsIndicators: true, content: {
-            VStack {
-                Spacer()
-                ForEach(routines, id: \.self) { routine in
-                    RoutineItemView(routine: routine)
-                        .padding(.all, 5)
+        ZStack{
+            ScrollView(.vertical, showsIndicators: true, content: {
+                VStack {
+                    Spacer()
+                    ForEach(routines, id: \.self) { routine in
+                        RoutineItemView(routine: routine)
+                            .padding(.all, 5)
+                    }
+                    Button(action: addButtonPressed, label: {
+                        Text("Add Routine")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .frame(width: 350, height: 70)
+                            .background(Color.accentColor)
+                            .cornerRadius(15)
+                    })
                 }
-                Button(action: addButtonPressed, label: {
-                    Text("Add Routine")
-                        .foregroundColor(.white)
-                        .font(.headline)
-                        .frame(width: 350, height: 70)
-                        .background(Color.accentColor)
-                        .cornerRadius(15)
+                
+                .sheet(isPresented: $addRoutine, content: {
+                    AddRoutineScreen()
                 })
-            }
-        })
-        .navigationBarBackButtonHidden(false)
-        .navigationTitle("Workout Days")
-        .navigationBarItems(
-            //leading: BackButton()
-            trailing: EditButton()
-        )
+                /*
+                if addRoutine {
+                    AddRoutineScreen()
+                        .transition(.move(edge: .bottom))
+                }*/
+            })
+            .navigationBarBackButtonHidden(false)
+            .navigationTitle("Workout Days")
+            .navigationBarItems(
+                //leading: BackButton()
+                trailing: EditButton()
+            )
+        }
     }
     
     func addButtonPressed() {
-        
+        addRoutine.toggle()
+    }
+}
+
+struct AddRoutineScreen: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @State var textFieldText: String  = ""
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                TextField("Type your new routine", text: $textFieldText)
+                    .padding(.horizontal)
+                    .frame(height: 55)
+                    .background(Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)))
+                    .cornerRadius(10)
+                Button(action: saveButtonPressed, label: {
+                    Text("Save")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                })
+            }
+        }
+        .padding()
+        .navigationTitle("Add an item")
     }
 }
 
@@ -51,6 +93,11 @@ struct RoutineSelectionVIew_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
             RoutineSelectionVIew()
+            //AddRoutineScreen()
         }
     }
+}
+
+func saveButtonPressed() {
+    
 }
