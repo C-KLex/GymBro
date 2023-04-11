a645ace6e42a9b4cf285c2bb5b20a961fc96eb6e
Yi Lee <87312700+YLee2121@users.noreply.github.com>
2023-04-11 19:37:52 -0400

    MainView first PR (#5)
    * construct five main tab
    
    * MainView setup, Add intro View for five tabs
    
    * Update the MainView:
    	- create a enum Tab
    	- separate the tabview into a independent view and add the tag
    
    * organized the file again, it just happends
    
    * documentation on MainView
    
    * documentation on MainView
    
    Embed all the tab into a NavigationView
    
    ---------
    
    Co-authored-by: Alex Chao <alex308248@gmail.com>

--- GymBro/Views/MainView.swift
+++ GymBro/Views/MainView.swift
@@ -10,14 +10,102 @@
 
 import SwiftUI
 
+
+
+/// The very first view after starting the APP
 struct MainView: View {
+    
+    /// The control variable for the FiveTabView
+    ///
+    /// It will change the defualt view of the tab view inside the FiveTabView
+    @State private var selectedTab: Tab = .Routine
+    
+    
+    // MARK: BODY
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        FiveTabView(selectedTab: selectedTab)
     }
 }
+
+
+// MARK: PREVIEW
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
+    }
+}
+
+
+// MARK: COMPONENT
+
+/// the enum type for the Tab of tab bar
+///
+/// - Warning: I will keep this enum Tab in MainView for a while for researching a better arrangement
+enum Tab {
+    case Chart
+    case History
+    case Routine
+    case Info
+    case Setting
+}
+
+/// The tab view for five modules
+///
+/// Thing to do:
+/// 1. How to extend the showing View from only one View to multiple view in one tab
+/// - Warning: I will keep the view in the MainView for a while for a better arrangement!
+struct FiveTabView: View {
+
+    /// Tab controler
+    ///
+    /// A required parameter when init FiveTabView
+    @State var selectedTab: Tab
+
+    var body: some View {
+        TabView(selection: $selectedTab) {
+
+            NavigationView {
+                MainChartView()
+            }
+            .tabItem {
+                Label("Chart", systemImage: "chart.xyaxis.line")
+            }
+            .tag(Tab.Chart)
+
+            NavigationView {
+                IntroHistoryView()
+            }
+            .tabItem {
+                Label("Histroy", systemImage: "book")
+            }
+            .tag(Tab.History)
+
+            NavigationView {
+                NewRoutineView()
+            }
+            .tabItem {
+                Label("Routine", systemImage: "dumbbell")
+            }
+            .tag(Tab.Routine)
+
+            NavigationView {
+                MainInfoView()
+            }
+            .tabItem {
+                Label("Info", systemImage: "person.fill")
+            }
+            .tag(Tab.Info)
+
+            NavigationView {
+                MainSettingView()
+            }
+            .tabItem {
+                Label("Setting", systemImage: "gear")
+            }
+            .tag(Tab.Setting)
+
+        }
     }
 }
---
+++ GymBro/Views/SettingViews/MainSettingView.swift
@@ -0,0 +1,20 @@
+//
+//  MainSettingView.swift
+//  GymBro
+//
+//  Created by user236772 on 4/7/23.
+//
+
+import SwiftUI
+
+struct MainSettingView: View {
+    var body: some View {
+        Text("SETTING")
+    }
+}
+
+struct MainSettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainSettingView()
+    }
+}
