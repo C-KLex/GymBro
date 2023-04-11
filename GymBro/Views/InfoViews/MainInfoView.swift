83a640558893273138fc27c7b7d3bed8d5efd220
Alex Chao <44662448+alex308248@users.noreply.github.com>
2023-04-11 19:38:44 -0400

    Version1 adding .gitignore and deleting ViewModels and Models folder (#4)
    * Remove files in ViewModels folder & Models folder
    
    * Add a .gitignore file and remove the revelented files from branch
    
    * MainView first PR (#5)
    
    * construct five main tab
    
    * MainView setup, Add intro View for five tabs
    
    * Update the MainView:
    	- create a enum Tab
    	- separate the tabview into a independent view and add the tag
    
    * organized the file again, it just happends
    
    * documentation on MainView
    
    * documentation on MainView
    
    Embed all the tab into a NavigationView
    
    ---------
    
    Co-authored-by: Alex Chao <alex308248@gmail.com>
    
    ---------
    
    Co-authored-by: Alex Chao <kylelee@gapp.nthu.edu.tw>
    Co-authored-by: Yi Lee <87312700+YLee2121@users.noreply.github.com>

---
+++ GymBro/Views/InfoViews/MainInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  MainInfoView.swift
+//  GymBro
+//
+//  Created by user236772 on 4/9/23.
+//
+
+import SwiftUI
+
+struct MainInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainInfoView()
+    }
+}
