57b61c299d516b298d23940bc0b283eab7085fd0
I-Wei Chen <i4chen@ucsd.edu>
2023-04-12 08:58:00 -0700

    Version1 RoutineExerciseView First PR

---
+++ GymBro/Views/RoutineViews/AddExerciseButton.swift
@@ -0,0 +1,20 @@
+//
+//  AddExerciseButton.swift
+//  GymBro
+//
+//  Created by 陳宜葳 on 4/11/23.
+//
+
+import SwiftUI
+
+struct AddExerciseButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddExerciseButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AddExerciseButton()
+    }
+}
