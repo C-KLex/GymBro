b3fd2ee372cf20e225dcd57cd3619e4f6d9247ee
YLee2121 <kylelee@gapp.nthu.edu.tw>
2023-04-13 14:08:45 -0400

    intro history view first

--- GymBro/Views/HistoryViews/IntroHistoryView.swift
+++ GymBro/Views/HistoryViews/IntroHistoryView.swift
@@ -5,16 +5,72 @@
 //  Created by user236772 on 4/9/23.
 //
 
+
+
+
 import SwiftUI
 
 struct IntroHistoryView: View {
+    
+    @State private var items = ["Item 1", "Item 2", "Item 3"]
+    
     var body: some View {
-        Text("History")
+        
+        VStack {
+            List {
+                ForEach(items, id: \.self) { item in
+                    Text(item)
+                        .swipeActions(edge: .trailing, allowsFullSwipe: false) {
+                            
+                            Button(
+                                action: {
+                                    deleteRoutine()
+                                },
+                                label: {
+                                    Label("Delete", systemImage: "trash")
+                                    
+                                }
+                            )
+                            .tint(.red)
+                            
+                            Button(
+                                action: {
+                                    editRoutine()
+                                },
+                                label: {
+                                    Label("Edit", systemImage: "restart")
+                                }
+                            )
+                            .tint(.green)
+                        }
+                }
+            }
+            
+            Button("hi") {
+                print("hi")
+            }
+            
+        }
+        .navigationTitle("History")
+        
+     
+
+    }
+ 
+    func deleteRoutine() {
+        print("delete")
+    }
+    func editRoutine() {
+        print("edit")
+        
     }
 }
 
+
 struct IntroHistoryView_Previews: PreviewProvider {
     static var previews: some View {
-        IntroHistoryView()
+        NavigationView {
+            IntroHistoryView()
+        }
     }
 }
