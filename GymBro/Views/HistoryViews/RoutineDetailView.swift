6623c26da4d40c79329098dbb516eb11ff20135e
YLee2121 <kylelee@gapp.nthu.edu.tw>
2023-04-13 18:21:31 -0400

    Draft the IntroHistroyView

---
+++ GymBro/Views/HistoryViews/RoutineDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  RoutineDetailView.swift
+//  GymBro
+//
+//  Created by user236772 on 4/14/23.
+//
+
+import SwiftUI
+
+struct RoutineDetailView: View {
+    var body: some View {
+        Text("Routine Detail View")
+    }
+}
+
+struct RoutineDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        RoutineDetailView()
+    }
+}
