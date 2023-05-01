d3f41dc2da017e4550ec63826b42be7d47a2f9f1
I-Wei Chen <i4chen@ucsd.edu>
2023-05-01 09:05:54 -0700

    Add EditSetView and upgrade RoutineExerciseView

---
+++ GymBro/Views/RoutineViews/NoExerciseView.swift
@@ -0,0 +1,20 @@
+//
+//  NoExerciseView.swift
+//  GymBro
+//
+//  Created by 陳宜葳 on 4/28/23.
+//
+
+import SwiftUI
+
+struct NoExerciseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoExerciseView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoExerciseView()
+    }
+}
