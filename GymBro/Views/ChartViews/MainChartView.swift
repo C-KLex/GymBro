acad0b0e4841485b686318a16238bb31c9b12d49
Alex Chao <alex308248@gmail.com>
2023-06-11 14:39:39 -0400

    help you commit first

--- GymBro/Views/ChartViews/MainChartView.swift
+++ GymBro/Views/ChartViews/MainChartView.swift
@@ -6,10 +6,78 @@
 //
 
 import SwiftUI
+import Charts
 
 struct MainChartView: View {
     var body: some View {
-        Text("CHART")
+        
+        let benchPress = [ weightData(year:2023, month:1, day: 1, weight: 100),
+                           weightData(year:2023, month:1, day: 5, weight: 105),
+                           weightData(year:2023, month:1, day: 9, weight: 110),
+                           weightData(year:2023, month:1, day: 13, weight: 115),
+                           weightData(year:2023, month:1, day: 17, weight: 120),
+                           weightData(year:2023, month:1, day: 21, weight: 125),
+                           weightData(year:2023, month:1, day: 25, weight: 130),
+                           weightData(year:2023, month:1, day: 29, weight: 130),
+                           weightData(year:2023, month:2, day: 2, weight: 135),
+                           weightData(year:2023, month:2, day: 6, weight: 140)]
+        
+        let squat = [ weightData(year:2023, month:1, day: 2, weight: 200),
+                           weightData(year:2023, month:1, day: 6, weight: 205),
+                           weightData(year:2023, month:1, day: 10, weight: 210),
+                           weightData(year:2023, month:1, day: 14, weight: 215),
+                           weightData(year:2023, month:1, day: 18, weight: 215),
+                           weightData(year:2023, month:1, day: 22, weight: 220),
+                           weightData(year:2023, month:1, day: 26, weight: 215),
+                           weightData(year:2023, month:1, day: 30, weight: 220),
+                           weightData(year:2023, month:2, day: 3, weight: 225),
+                           weightData(year:2023, month:2, day: 7, weight: 230)]
+        
+        
+        let chartData = [ (exercise: "Bench Press", data: benchPress),
+                          (exercise: "Squat", data: squat)
+        ]
+        
+        VStack {
+            Chart {
+                ForEach(chartData, id: \.exercise) { series in
+                    ForEach(series.data) { item in
+                        LineMark(
+                            x: .value("Date", item.date),
+                            y: .value("Weight", item.weight)
+                        )
+                    }
+                    .foregroundStyle(by: .value("Exercise", series.exercise))
+                    .symbol(by: .value("Exercise", series.exercise))
+                }
+            }
+            .chartXAxis {
+                AxisMarks(values: .stride(by: .month)) { value in
+                    AxisGridLine()
+                    AxisValueLabel(format: .dateTime.month(.defaultDigits))
+                }
+            }
+            /*
+            .chartPlotStyle { plotArea in
+                plotArea
+                    .background(.blue.opacity(0.1))
+            }
+             */
+        
+        }
+        .padding()
+        
+    }
+}
+
+struct weightData: Identifiable {
+    let id = UUID()
+    let date: Date
+    let weight: Double
+
+    init(year: Int, month: Int, day: Int, weight: Double) {
+        self.date = Calendar.current.date(from: .init(year: year, month: month, day: day)) ?? Date()
+        self.weight = weight
     }
 }
 
