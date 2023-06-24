cdcff019b83ff7cfb44e67a25f2daf67a1a79410
Alex Chao <alex308248@gmail.com>
2023-06-23 22:38:56 -0400

    HELP YOU COMMIT TO SAVE CHANGES

--- GymBro/Views/ChartViews/ChartView.swift
+++ GymBro/Views/ChartViews/ChartView.swift
@@ -1,14 +1,14 @@
 //
-//  MainChartView.swift
+//  ChartView.swift
 //  GymBro
 //
-//  Created by user236772 on 4/7/23.
+//  Created by user239615 on 6/21/23.
 //
 
 import SwiftUI
 import Charts
 
-struct MainChartView: View {
+struct ChartView: View {
     
     let benchPress = [ weightData(year:2023, month:1, day: 1, weight: 100),
                        weightData(year:2023, month:1, day: 5, weight: 105),
@@ -32,35 +32,28 @@ struct MainChartView: View {
                        weightData(year:2023, month:2, day: 3, weight: 225),
                        weightData(year:2023, month:2, day: 7, weight: 230)]
     
-    
-    
-    var chartData:[(exerciseName: String, data: [weightData], pick: Bool)] = []
-    
-    init() {
-        chartData.append((exerciseName: "Bench Press", data: benchPress, pick: true))
-        chartData.append((exerciseName: "Squat", data: squat, pick: false))
-    }
-    
-    @State var exercises: [String] = [
-        "benchPress",
-        "squat"
-    ]
+    let chartData = [
+        exerciseData(exerciseName: "Bench Press", data: benchPress, pick: true),
+        exerciseData(exerciseName: "Squats", data: squat, pick: true)]
     
     var body: some View {
-        
         VStack {
-            /*
+            
+            ForEach(chartData, id: \.self) { series in
+                Text(String(1233))
+            }
+            
             GroupBox() {
-            //GroupBox ( "Line Chart - Weight Growing") {
+                //GroupBox ( "Line Chart - Weight Growing") {
                 Chart {
-                    ForEach(chartData, id: \.exercise) { series in
+                    ForEach(chartData, id: \.exerciseName) { series in
                         ForEach(series.data) { item in
                             LineMark(
                                 x: .value("Date", item.date),
                                 y: .value("Weight", item.weight)
                             )
-                            .foregroundStyle(by: .value("Exercise", series.exercise))
-                            .symbol(by: .value("Exercise", series.exercise))
+                            .foregroundStyle(by: .value("Exercise", series.exerciseName))
+                            .symbol(by: .value("Exercise", series.exerciseName))
                         }
                         
                     }
@@ -72,43 +65,67 @@ struct MainChartView: View {
                         AxisValueLabel(format: .dateTime.month(.defaultDigits))
                     }
                 }
-
-                 
-                
             }
             .groupBoxStyle(WhiteGroupBoxStyle())
             .padding()
-            */
+            
             Spacer()
-            /*
-            List {
-                ForEach(exercises, id:\.self) { exercise in
-                    HStack {
-                        Image(systemName: exercise.choose ? "smallcircle.fill.circle" : "circle")
-                        Text(exercise)
-                    }
-                }
-            }
             
             List {
-                ForEach(chartData, id:\.self) { exercise in
+                ForEach(chartData, id:\.exerciseName) { exercise in
                     HStack {
                         Image(systemName: exercise.pick ? "smallcircle.fill.circle" : "circle")
                         Text(exercise.exerciseName)
                     }
                 }
             }
-             */
         }
         .navigationTitle("Charts")
-        
     }
 }
 
-struct MainChartView_Previews: PreviewProvider {
+struct WhiteGroupBoxStyle: GroupBoxStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.content
+            .padding(.top, 30)
+            .padding(20)
+            .background(Color.LightGray())
+            .cornerRadius(20)
+            .overlay(
+                configuration.label.padding(10),
+                alignment: .topLeading
+            )
+    }
+}
+
+struct weightData: Identifiable {
+    let id = UUID()
+    let date: Date
+    let weight: Double
+
+    init(year: Int, month: Int, day: Int, weight: Double) {
+        self.date = Calendar.current.date(from: .init(year: year, month: month, day: day)) ?? Date()
+        self.weight = weight
+    }
+}
+
+struct exerciseData: Identifiable {
+    let id = UUID()
+    let exerciseName: String
+    let data = [weightData(year: <#Int#>, month: <#Int#>, day: <#Int#>, weight: <#Double#>)]
+    let pick: Bool
+    
+    init(exerciseName: String, data: [weightData], pick: Bool) {
+        self.exerciseName = exerciseName
+        self.data = data
+        self.pick = pick
+    }
+}
+
+struct ChartView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            MainChartView()
+            ChartView()
         }
     }
 }
