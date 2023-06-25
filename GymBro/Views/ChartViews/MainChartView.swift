9eaf6cb5dd092658b83211f9e218b45b527723fc
Alex Chao <alex308248@gmail.com>
2023-06-24 23:23:35 -0400

    A working verison QAQ

--- GymBro/Views/ChartViews/MainChartView.swift
+++ GymBro/Views/ChartViews/MainChartView.swift
@@ -10,31 +10,40 @@ import Charts
 
 struct MainChartView: View {
     
-    let benchPress = [ weightData(year:2023, month:1, day: 1, weight: 100),
-                       weightData(year:2023, month:1, day: 5, weight: 105),
-                       weightData(year:2023, month:1, day: 9, weight: 110),
-                       weightData(year:2023, month:1, day: 13, weight: 115),
-                       weightData(year:2023, month:1, day: 17, weight: 120),
-                       weightData(year:2023, month:1, day: 21, weight: 125),
-                       weightData(year:2023, month:1, day: 25, weight: 130),
-                       weightData(year:2023, month:1, day: 29, weight: 130),
-                       weightData(year:2023, month:2, day: 2, weight: 135),
-                       weightData(year:2023, month:2, day: 6, weight: 140)]
+    let benchPress: [weightData] = [
+        weightData(year:2023, month:1, day: 1, weight: 100),
+        weightData(year:2023, month:1, day: 5, weight: 105),
+        weightData(year:2023, month:1, day: 9, weight: 110),
+        weightData(year:2023, month:1, day: 13, weight: 115),
+        weightData(year:2023, month:1, day: 17, weight: 120),
+        weightData(year:2023, month:1, day: 21, weight: 125),
+        weightData(year:2023, month:1, day: 25, weight: 130),
+        weightData(year:2023, month:1, day: 29, weight: 130),
+        weightData(year:2023, month:2, day: 2, weight: 135),
+        weightData(year:2023, month:2, day: 6, weight: 140)
+    ]
     
-    let squat = [ weightData(year:2023, month:1, day: 2, weight: 200),
-                       weightData(year:2023, month:1, day: 6, weight: 205),
-                       weightData(year:2023, month:1, day: 10, weight: 210),
-                       weightData(year:2023, month:1, day: 14, weight: 215),
-                       weightData(year:2023, month:1, day: 18, weight: 215),
-                       weightData(year:2023, month:1, day: 22, weight: 220),
-                       weightData(year:2023, month:1, day: 26, weight: 215),
-                       weightData(year:2023, month:1, day: 30, weight: 220),
-                       weightData(year:2023, month:2, day: 3, weight: 225),
-                       weightData(year:2023, month:2, day: 7, weight: 230)]
+    let squat: [weightData] = [
+        weightData(year:2023, month:1, day: 2, weight: 200),
+        weightData(year:2023, month:1, day: 6, weight: 205),
+        weightData(year:2023, month:1, day: 10, weight: 210),
+        weightData(year:2023, month:1, day: 14, weight: 215),
+        weightData(year:2023, month:1, day: 18, weight: 215),
+        weightData(year:2023, month:1, day: 22, weight: 220),
+        weightData(year:2023, month:1, day: 26, weight: 215),
+        weightData(year:2023, month:1, day: 30, weight: 220),
+        weightData(year:2023, month:2, day: 3, weight: 225),
+        weightData(year:2023, month:2, day: 7, weight: 230)
+    ]
     
-    let chartData = [
-        (exerciseName: "Bench Press", data: benchPress, pick: true),
-        (exerciseName: "Squats", data: squat, pick: true)]
+    let chartData: [(exerciseName: String, data: [weightData], pick: Bool)]
+        
+    init() {
+        chartData = [
+            (exerciseName: "Bench Press", data: benchPress, pick: true),
+            (exerciseName: "Squats", data: squat, pick: true)
+        ]
+    }
     
     var body: some View {
         VStack {
@@ -103,6 +112,7 @@ struct weightData: Identifiable {
         self.weight = weight
     }
 }
+
 
 struct MainChartView_Previews: PreviewProvider {
     static var previews: some View {
